08695c0bca41fabb2c6734ad7647f63a87fffabf
HHansi <h.hansi1991@gmail.com>
2022-04-12 22:43:44 +0100

    006: Updated config

--- experiments/document_classification/config.py
+++ experiments/document_classification/config.py
@@ -19,8 +19,8 @@ MODEL_NAME = "xlm-roberta-large"  # "dccuchile/bert-base-spanish-wwm-cased"  # "
 # MODEL_TYPE = "bert"
 # MODEL_NAME = "bert-large-cased"
 
-TRAIN_LANGUAGES = ["en"]
-TEST_LANGUAGES = ["en", "pr", "es", "hi"]
+TRAIN_LANGUAGES = ["pr"]
+TEST_LANGUAGES = ["pr"]
 
 BINARY_CLASS_BALANCE = False
 CLASS = 0  # will be used if BINARY_CLASS_BALANCE=True
@@ -48,14 +48,14 @@ config = {
     'max_grad_norm': 1.0,
     'do_lower_case': False,
 
-    'logging_steps': 200,  #40,
-    'save_steps': 200,  #40,
+    'logging_steps': 40,  #200,
+    'save_steps': 40,  #200,
     "no_cache": False,
     'save_model_every_epoch': True,
     "save_recent_only": True,
     'n_fold': 3,
     'evaluate_during_training': True,
-    'evaluate_during_training_steps': 200,  #40,
+    'evaluate_during_training_steps': 40,  #200,
     "evaluate_during_training_verbose": True,
     'use_cached_eval_features': False,
     'save_eval_checkpoints': True,
