22953f952c78bd163338f534bf7852d8bea815a8
HHansi <h.hansi1991@gmail.com>
2022-06-02 23:14:11 +0100

    008: HuggingFace setup

--- experiments/document_classification/huggingface_models.py
+++ experiments/document_classification/huggingface_models.py
@@ -1,14 +1,17 @@
 # Created by Hansi at 6/2/2022
 import torch
+from scipy.special import softmax
 
 from algo.classification.classification_model import ClassificationModel
-from algo.util.file_util import delete_create_folder
-from experiments.document_classification.config import TEMP_DIRECTORY
 
-delete_create_folder(TEMP_DIRECTORY)
+texts = [
+    'Police arrested five more student leaders on Monday when implementing the strike call given by MSU students union as a mark of protest against the decision to introduce payment seats in first-year commerce programme.']
 
-model = ClassificationModel('bigbird', 'EventMiner/bigbird-roberta-large-en-doc', use_cuda=torch.cuda.is_available(),
-                            use_auth_token=True)
-predictions, raw_predictions = model.predict(['Police arrested five more student leaders on Monday when implementing the strike call given by MSU students union as a mark of protest against the decision to introduce payment seats in first-year commerce programme.'])
-print(predictions)
-print(raw_predictions)
+model = ClassificationModel('bigbird', 'EventMiner/bigbird-roberta-large-en-doc', use_cuda=torch.cuda.is_available())
+predictions, raw_predictions = model.predict(texts)
+print(f'predictions: {predictions}')
+# print(f'raw predictions: {raw_predictions}')
+
+# convert raw predictions to probabilities
+raw_preds_probabilities = softmax(raw_predictions, axis=1)
+print(f'probabilities: {raw_preds_probabilities}')
