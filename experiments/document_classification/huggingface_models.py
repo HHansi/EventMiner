ddf895e326273f221a33dadad733928cafd87645
HHansi <h.hansi1991@gmail.com>
2022-06-19 16:09:04 +0100

    008: HuggingFace setup

--- experiments/document_classification/huggingface_models.py
+++ experiments/document_classification/huggingface_models.py
@@ -8,7 +8,7 @@ texts = [
     'Police arrested five more student leaders on Monday when implementing the strike call given by MSU students union as a mark of protest against the decision to introduce payment seats in first-year commerce programme.']
 
 # model = ClassificationModel('bigbird', 'EventMiner/bigbird-roberta-large-en-doc', use_cuda=torch.cuda.is_available())
-model = ClassificationModel('xlmroberta', 'EventMiner/EventMiner/xlm-roberta-large-en-pt-es-doc', use_cuda=torch.cuda.is_available())
+model = ClassificationModel('xlmroberta', 'EventMiner/xlm-roberta-large-en-pt-es-doc', use_cuda=torch.cuda.is_available())
 predictions, raw_predictions = model.predict(texts)
 print(f'predictions: {predictions}')
 # print(f'raw predictions: {raw_predictions}')
